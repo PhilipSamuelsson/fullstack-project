e6ae707d5a911f6869f1e752f06e0088b3f12825
Dennis Eghtedari <year_2000_@hotmail.com>
2023-05-23 21:21:19 +0100

    började med formulär komponenten.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar/NavBar'
 import HeroImage from '../components/HeroImage/HeroImage'
+import Bookingform from '../components/bookingform/bookingform'
 
 function HomePage() {
     return (
         <div>
+            <Bookingform />
             <NavBar />
             <HeroImage/>
         </div>
