639d760b4d9488b908681c276ec0beff291e9cf7
Emelie Widén <emelie.widen@icloud.com>
2023-05-29 12:49:20 +0200

    .

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -10,50 +10,50 @@ function Footer() {
 				<div className="col-xs-12 col-sm-4 col-md-4">
 					<h5>OM OSS</h5>
 					<ul className="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Om oss</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Lediga jobb</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Vår planet</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Våra medarbetare</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Frågor och svar</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Om oss</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Lediga jobb</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Vår planet</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Våra medarbetare</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Frågor och svar</a></li>
 					</ul>
 				</div>
 				<div className="col-xs-12 col-sm-4 col-md-4">
 					<h5>BOKA FLYG</h5>
 					<ul className="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Boka flyg</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Resetjänster</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Transport</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Planera din resa</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Flygstatus</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Boka flyg</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Resetjänster</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Transport</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Planera din resa</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Flygstatus</a></li>
 					</ul>
 				</div>
                 <div className="col-xs-12 col-sm-4 col-md-4">
 					<h5>INNAN DU FLYGER</h5>
 					<ul className="list-unstyled quick-links">
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Bagage</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Checka in</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Hantera bokning</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Passinformation</a></li>
-						<li><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Hälsa</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Bagage</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Checka in</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Hantera bokning</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Passinformation</a></li>
+						<li><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-angle-double-right"></i>Hälsa</a></li>
 					</ul>
 				</div>
 			</div>
 			<div className="row">
 				<div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
 					<ul className="list-unstyled list-inline social text-center">
-						<li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a></li>
-						<li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a></li>
-						<li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a></li>
-						<li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-google-plus"></i></a></li>
-						<li className="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank" rel="noopener noreferrer"><i className="fa fa-envelope"></i></a></li>
+						<li className="list-inline-item"><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a></li>
+						<li className="list-inline-item"><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a></li>
+						<li className="list-inline-item"><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a></li>
+						<li className="list-inline-item"><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-google-plus"></i></a></li>
+						<li className="list-inline-item"><a href="#" target="_blank" rel="noopener noreferrer"><i className="fa fa-envelope"></i></a></li>
 					</ul>
 				</div>
 				<hr></hr>
 			</div>
 			<div className="row">
 				<div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p><u><a href="https://www.nationaltransaction.com/">AirVenture</a></u> Trekantsvägen 1, 12059 Stockholm</p>
-					<p className="h6">© All right Reversed.<a className="text-green ml-2" href="https://www.sunlimetech.com"target="_blank" rel="noopener noreferrer">AirVenture</a></p>
+					<p><u><a href="#">AirVenture</a></u> Trekantsvägen 1, 12059 Stockholm</p>
+					<p className="h6">© All right Reversed.<a className="text-green ml-2" href="#"target="_blank" rel="noopener noreferrer">AirVenture</a></p>
 				</div>
 				<hr></hr>
 			</div>
