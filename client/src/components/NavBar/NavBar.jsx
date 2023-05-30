18e657ba3778cabcebbc71a2c49996fc9d759f5b
PhilipSamuelsson <philip.samuelsson@iths.se>
2023-05-30 11:43:19 +0200

    123

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,77 +1,47 @@
-import React from 'react'
-import { useEffect } from 'react'
-import './NavBar.css'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
 
 function NavBarTest() {
-    useEffect(() => {
-        const navTrigger = document.querySelector('.navTrigger')
-        const mainListDiv = document.querySelector('#mainListDiv')
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-        navTrigger.addEventListener('click', function () {
-            this.classList.toggle('active')
-            console.log('Clicked menu')
-            mainListDiv.classList.toggle('show_list')
-            if (mainListDiv.classList.contains('show_list')) {
-                mainListDiv.style.display = 'block'
-                mainListDiv.style.opacity = '1'
-            } else {
-                mainListDiv.style.opacity = '0'
-                setTimeout(() => {
-                    mainListDiv.style.display = 'none'
-                }, 300) // Fade out duration is 300ms
-            }
-        })
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-        return () => {
-            navTrigger.removeEventListener('click', () => {
-                this.classList.toggle('active')
-                console.log('Clicked menu')
-                mainListDiv.classList.toggle('show_list')
-                if (mainListDiv.classList.contains('show_list')) {
-                    mainListDiv.style.display = 'block'
-                    mainListDiv.style.opacity = '1'
-                } else {
-                    mainListDiv.style.opacity = '0'
-                    setTimeout(() => {
-                        mainListDiv.style.display = 'none'
-                    }, 300) // Fade out duration is 300ms
-                }
-            })
-        }
-    }, [])
-
-    return (
-        <nav className="nav">
-            <div className="containerNav">
-                <div className="logo">
-                    {/* <h2>AirVenture</h2> */}
-                </div>
-                <div id="mainListDiv" className="main_list">
-                    <ul className="navlinks">
-                        <li>
-                            <Link to="/">HEM</Link>
-                        </li>
-
-                        <li>
-                            <Link to="/bookings">BOKA</Link>
-                        </li>
-                        <li>
-                            <Link to="/avgångar">AVGÅNGAR</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">KONTAKT</Link>
-                        </li>
-                    </ul>
-                </div>
-                <span className="navTrigger">
-                    <i></i>
-                    <i></i>
-                    <i></i>
-                </span>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="nav">
+      <div className="containerNav">
+        <div className="logo">
+          {/* <h2>AirVenture</h2> */}
+        </div>
+        <div
+          id="mainListDiv"
+          className={`main_list ${isMenuOpen ? 'show_list' : ''}`}
+        >
+          <ul className="navlinks">
+            <li>
+              <Link to="/">HEM</Link>
+            </li>
+            <li>
+              <Link to="/bookings">BOKA</Link>
+            </li>
+            <li>
+              <Link to="/departures">AVGÅNGAR</Link>
+            </li>
+            <li>
+              <Link to="/contact">KONTAKT</Link>
+            </li>
+          </ul>
+        </div>
+        <span className="navTrigger" onClick={handleMenuClick}>
+          <i></i>
+          <i></i>
+          <i></i>
+        </span>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBarTest
+export default NavBarTest;
